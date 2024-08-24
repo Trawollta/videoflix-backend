5bd7b479e801e9a740ff39fe85e3e348a04010b1
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-08-25 01:27:46 +0200

    changes in allowed hosts

--- videoflix_app/settings.py
+++ videoflix_app/settings.py
@@ -11,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(','), 'jan-woll.developerakademie.org'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') + ['jan-woll.developerakademie.org']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -117,6 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
