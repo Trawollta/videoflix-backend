479f63f11e1bb3e0cf5287b59fecb05674ddd9f4
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-08-25 23:41:25 +0200

    debug

--- videoflix_app/settings.py
+++ videoflix_app/settings.py
@@ -10,7 +10,7 @@ os.environ['SSL_CERT_FILE'] = certifi.where()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 if ALLOWED_HOSTS:
