fb9eb229a677d5bcfbadd2b18ab318c76e784e94
Jan Woll <144726069+Trawollta@users.noreply.github.com>
2024-08-27 17:15:49 +0200

    changes

--- videoflix_app/settings.py
+++ videoflix_app/settings.py
@@ -3,28 +3,30 @@ import certifi
 from pathlib import Path
 from dotenv import load_dotenv
 
+# .env-Datei laden
 load_dotenv()
 
+# Zertifikatspfad festlegen
 os.environ['SSL_CERT_FILE'] = certifi.where()
 
+# Basisverzeichnis festlegen
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Sicherheitsrelevante Einstellungen
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
-if ALLOWED_HOSTS:
-    ALLOWED_HOSTS = ALLOWED_HOSTS.split(',')
-else:
+# ALLOWED_HOSTS aus der Umgebungsvariable laden
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+if not ALLOWED_HOSTS or ALLOWED_HOSTS == ['']:
     raise ValueError("Die Umgebungsvariable 'ALLOWED_HOSTS' ist nicht gesetzt.")
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://jan-woll.developerakademie.org',
-    'http://videoflix.jan-woll.de',
-]
+# CSRF Trusted Origins
+CSRF_TRUSTED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+if not CSRF_TRUSTED_ORIGINS or CSRF_TRUSTED_ORIGINS == ['']:
+    raise ValueError("Die Umgebungsvariable 'CORS_ALLOWED_ORIGINS' ist nicht gesetzt.")
 
-
-
+# Installierte Apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'user'
 ]
 
+# Django RQ Einstellungen für Redis
 RQ_QUEUES = {
     'default': {
         'HOST': os.getenv('REDIS_HOST', 'localhost'),
@@ -49,14 +52,17 @@ RQ_QUEUES = {
     },
 }
 
+# Benutzerdefiniertes User Model
 AUTH_USER_MODEL = 'user.CustomUser'
 
+# REST Framework Einstellungen
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
+# Middleware-Einstellungen
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,16 +74,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS')
-
-if CORS_ALLOWED_ORIGINS:
-    CORS_ALLOWED_ORIGINS = CORS_ALLOWED_ORIGINS.split(',')
-else:
+# CORS Einstellungen
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
+if not CORS_ALLOWED_ORIGINS or CORS_ALLOWED_ORIGINS == ['']:
     raise ValueError("Die Umgebungsvariable 'CORS_ALLOWED_ORIGINS' ist nicht gesetzt.")
 
-
+# URL-Konfiguration
 ROOT_URLCONF = 'videoflix_app.urls'
 
+# Templates-Einstellungen
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -94,6 +99,7 @@ TEMPLATES = [
     },
 ]
 
+# Caching Einstellungen
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
@@ -105,8 +111,10 @@ CACHES = {
     }
 }
 
+# WSGI-Anwendung
 WSGI_APPLICATION = 'videoflix_app.wsgi.application'
 
+# Datenbank-Einstellungen
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -114,6 +122,7 @@ DATABASES = {
     }
 }
 
+# Passwortvalidierung
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -129,21 +138,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Internationale Einstellungen
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Statische Dateien
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Medien-Dateien
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Standard AutoField
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# E-Mail-Konfiguration
+# E-Mail Einstellungen
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'your-default-email-host')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
